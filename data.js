61ff8c1a87a2303e7de676dd0ec25753ffba8313
takao yoshitomi <takaoyaji14@gmail.com>
2025-08-18 01:01:03 +0900

    jigyousya-kanri-ver1.0

--- data.js
+++ data.js
@@ -11,7 +11,27 @@ function loadData() {
         { no: 103, name: "合同会社ベータ", fiscalMonth: "1月", unattendedMonths: "7ヵ月", monthlyProgress: "2026年1月迄",担当者: "鈴木", accountingMethod: "自計" , status: "完了" },
         { no: 201, name: "株式会社ガンマ", fiscalMonth: "2月", unattendedMonths: "6ヵ月", monthlyProgress: "2026年2月迄",担当者: "高橋", accountingMethod: "自計" , status: "完了" },
         { no: 301, name: "有限会社デルタ", fiscalMonth: "3月", unattendedMonths: "5ヵ月", monthlyProgress: "2026年3月迄",担当者: "田中", accountingMethod: "記帳代行" , status: "2チェック待ち" },
-        { no: 308, name: "株式会社イプシロン", fiscalMonth: "3月", unattendedMonths: "5ヵ月", monthlyProgress: "2026年3月迄",担当者: "渡辺", accountingMethod: "記帳代行" , status: "依頼中" }
+        { no: 308, name: "株式会社イプシロン", fiscalMonth: "3月", unattendedMonths: "5ヵ月", monthlyProgress: "2026年3月迄",担当者: "渡辺", accountingMethod: "記帳代行" , status: "依頼中" },
+        {"no":400,"name":"サンプル会社400","fiscalMonth":"1月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"佐藤","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":401,"name":"サンプル会社401","fiscalMonth":"2月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"鈴木","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":402,"name":"サンプル会社402","fiscalMonth":"3月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"高橋","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":403,"name":"サンプル会社403","fiscalMonth":"4月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"田中","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":404,"name":"サンプル会社404","fiscalMonth":"5月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"渡辺","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":405,"name":"サンプル会社405","fiscalMonth":"6月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"佐藤","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":406,"name":"サンプル会社406","fiscalMonth":"7月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"鈴木","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":407,"name":"サンプル会社407","fiscalMonth":"8月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"高橋","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":408,"name":"サンプル会社408","fiscalMonth":"9月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"田中","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":409,"name":"サンプル会社409","fiscalMonth":"10月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"渡辺","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":410,"name":"サンプル会社410","fiscalMonth":"11月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"佐藤","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":411,"name":"サンプル会社411","fiscalMonth":"12月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"鈴木","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":412,"name":"サンプル会社412","fiscalMonth":"1月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"高橋","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":413,"name":"サンプル会社413","fiscalMonth":"2月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"田中","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":414,"name":"サンプル会社414","fiscalMonth":"3月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"渡辺","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":415,"name":"サンプル会社415","fiscalMonth":"4月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"佐藤","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":416,"name":"サンプル会社416","fiscalMonth":"5月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"鈴木","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":417,"name":"サンプル会社417","fiscalMonth":"6月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"高橋","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":418,"name":"サンプル会社418","fiscalMonth":"7月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"田中","accountingMethod":"記帳代行","status":"未着手"},
+        {"no":419,"name":"サンプル会社419","fiscalMonth":"8月","unattendedMonths":"0ヵ月","monthlyProgress":"未開始","担当者":"渡辺","accountingMethod":"記帳代行","status":"未着手"}
     ];
 
     // 担当者リストをinitialClientsから自動生成
